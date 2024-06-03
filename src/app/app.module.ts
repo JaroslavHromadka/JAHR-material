5160f3d516ffac152f107697cccadde84894aa2f
JaroslavHromadka <jaroslav.hromadka@hotmail.com>
2024-06-03 08:44:40 +0200

    material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { OverlayModule } from '@angular/cdk/overlay'
+ 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    OverlayModule
   ],
   providers: [],
   bootstrap: [AppComponent]
